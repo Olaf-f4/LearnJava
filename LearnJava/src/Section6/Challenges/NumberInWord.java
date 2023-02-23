83c374747287eddb9e45262a672a16b6ed006642
Olaf <olafsenb@yahoo.com>
2023-02-23 12:13:44 -0500

    LearnJava 1.3

---
+++ LearnJava/src/Section6/Challenges/NumberInWord.java
@@ -0,0 +1,26 @@
+package Section6.Challenges;
+
+public class NumberInWord {
+
+    public static void printNumberInWord(int number) {
+        String num = switch (number) {
+            case 0 -> "ZERO";
+            case 1 -> "ONE";
+            case 2 -> "TWO";
+            case 3 -> "THREE";
+            case 4 -> "FOUR";
+            case 5 -> "FIVE";
+            case 6 -> "SIX";
+            case 7 -> "SEVEN";
+            case 8 -> "EIGHT";
+            case 9 -> "NINE";
+            default -> "other";
+
+        };
+        System.out.println(num);
+    }
+
+    public static void main(String[] args) {
+        printNumberInWord(223);
+    }
+}
